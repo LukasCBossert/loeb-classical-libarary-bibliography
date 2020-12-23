805870be63a7b198a137d5942600e762aa5207e4
Lukas C. Bossert <mail@lukascbossert.de>
2020-12-23 22:27:52 +0100

    first conversion done

--- lcl-convert.zsh
+++ lcl-convert.zsh
@@ -5,7 +5,7 @@
 file="loeb-classical-library-bibliography"
 file_out=${file}-out.bib
 
-awk -F "|" 'NR>1{printf "@book{%s,author={%s},title={%s},isbn={%s},subtitle={%s},origlanguage={%s},series={Loeb Classical Library},number={%s},pagetotal={%s},date={%s}}\n", $3,$5,$6,$3,$7,$8,$1,$15,$18 }' ${file}.csv >> ${file-out}
+# awk -F "|" 'NR>1{printf "@book{%s,author={%s},title={%s},isbn={%s},subtitle={%s},origlanguage={%s},series={Loeb Classical Library},number={%s},pagetotal={%s},date={%s}}\n", $3,$5,$6,$3,$7,$8,$1,$15,$18 }' ${file}.csv >> ${file-out}
 
 biber                                           \
 	--tool                                        \
