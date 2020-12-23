e12d237614c1e0f9a8ba62028e73c60aaac4f5c6
Lukas C. Bossert <mail@lukascbossert.de>
2020-12-23 23:01:49 +0100

    improved conversion

--- lcl-convert.zsh
+++ lcl-convert.zsh
@@ -1,18 +1,15 @@
 #!/bin/bash
-#Generate a BibTeX bibliography from an array of ISBNs
-
 
 file="loeb-classical-library-bibliography"
-file_out=${file}-out.bib
+file_out="${file}-out"
 
-# awk -F "|" 'NR>1{printf "@book{%s,author={%s},title={%s},isbn={%s},subtitle={%s},origlanguage={%s},series={Loeb Classical Library},number={%s},pagetotal={%s},date={%s}}\n", $3,$5,$6,$3,$7,$8,$1,$15,$18 }' ${file}.csv >> ${file-out}
+awk -F "|" 'NR>1{printf "@book{%s,author={%s},title={%s},isbn={%s},subtitle={%s},origlanguage={%s},series={Loeb Classical Library},number={%s},pagetotal={%s},date={%s}}\n", $3,$5,$6,$3,$7,$8,$1,$15,$18 }' ${file}.csv >> ${file-out}.bib
 
 biber                                           \
 	--tool                                        \
 	--quiet                                       \
 	--output-field-order=author,title,journaltitle,date,year \
-	--outfile "${file-out}"                              \
+	--outfile "${file-out}.bib"                              \
 	--configfile="loeb-bibtex.conf"          \
-	"${file-out}"
-	rm "${file-out}".blg
-
+	"${file-out}.bib"
+	rm "${file-out}.bib.blg"
