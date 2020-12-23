ea6551912beb1f6dce8e7c20ac74cda436a095e9
Lukas C. Bossert <mail@lukascbossert.de>
2020-12-23 23:34:09 +0100

    two separate bibtex-files

--- lcl-convert.zsh
+++ lcl-convert.zsh
@@ -3,7 +3,9 @@
 file="loeb-classical-library-bibliography"
 file_out="${file}-out"
 
-awk -F "|" 'NR>1{printf "@book{%s,author={%s},title={%s},isbn={%s},subtitle={%s},origlanguage={%s},series={Loeb Classical Library},number={%s},pagetotal={%s},date={%s}}\n", $3,$5,$6,$3,$7,$8,$1,$15,$18 }' ${file}.csv >> ${file-out}.bib
+ancient="ancient-authors-and-works.bib"
+
+# awk -F "|" 'NR>1{printf "@book{%s,author={%s},title={%s},isbn={%s},subtitle={%s},origlanguage={%s},series={Loeb Classical Library},number={%s},pagetotal={%s},date={%s}}\n", $3,$5,$6,$3,$7,$8,$1,$15,$18 }' ${file}.csv >> ${file-out}.bib
 
 biber                                           \
 	--tool                                        \
@@ -13,3 +15,15 @@ biber                                           \
 	--configfile="loeb-bibtex.conf"          \
 	"${file-out}.bib"
 	rm "${file-out}.bib.blg"
+
+
+biber                                           \
+	--tool                                        \
+	--quiet                                       \
+	--output-resolve \
+	--output-field-order=author,title,journaltitle,date,year \
+	--outfile "${ancient}"                              \
+	--configfile="loeb-bibtex.conf"          \
+	"${ancient}"
+	rm "${ancient}.blg"
+
